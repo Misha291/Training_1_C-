2e1090e8cfc82a7d34ec81f839de3abad9d815b2
Михаил Захаров <z27misha@gmail.com>
2026-02-03 23:39:43 +0500

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace tran2
+{
+    class Program
+    {
+        public static void Main()
+        {
+           Console.Write("Введите цену товара:");
+           double price = double.Parse(Console.ReadLine());
+
+           Console.Write("Введите скидку в процентах:");
+           double sale = double.Parse(Console.ReadLine());
+
+           double itog_price = price * (1 - sale / 100);
+           Console.Write($"Итоговая цена:{itog_price:F2}");
+
+           Console.ReadKey();
+        }
+    }
+}
